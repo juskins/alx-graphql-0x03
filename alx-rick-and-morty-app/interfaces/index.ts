bc0614442be850b108f663d63e2003f1068e54ec
juskins <omojuwababatunde1@gmail.com>
2025-12-24 23:01:54 +0100

    first commit

---
+++ alx-rick-and-morty-app/interfaces/index.ts
@@ -0,0 +1,23 @@
+interface InfoProps {
+    pages: number
+    next: number
+    prev: number
+    count: number
+}
+
+export interface EpisodeProps {
+  id: number
+  name: string
+  air_date: string
+  episode: string
+  info: InfoProps
+}
+
+export interface EpisodesQueryData {
+  episodes: {
+    info: InfoProps;
+    results: EpisodeProps[];
+  };
+}
+
+export type EpisodeCardProps = Pick<EpisodeProps, 'id' | 'name'| 'air_date' | "episode">
